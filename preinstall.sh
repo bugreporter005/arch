34aafe0a345419f2f498a3460fc7da1fa1dd4126
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-24 04:11:43 +0500

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -7,26 +7,7 @@
 #!/bin/bash
 
 
-console_font="ter-v18n"
-
-wifi_interface="wlan0"
-wifi_SSID=""
-wifi_passphrase=""
-
-drive="/dev/vda" # run 'lsblk'
-efi_partition="${drive}1"
-root_partition="${drive}2"
-
-luks_label="cryptroot"
-luks_passphrase=""
-
-hostname="arch"
-timezone="" # run 'timedatectl list-timezones'
-username=""
-user_passphrase=""
-
-
-
+source variables.sh
 
 # Set a bigger font
 setfont $console_font
