9f6b68d28a74bc4e56b3e8316a28aa12f8a19d7f
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-24 18:40:45 +0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 
+console_font="ter-v18n"
+
+wifi_interface="wlan0"
+wifi_SSID=""
+wifi_passphrase=""
+
+drive="/dev/vda" # run 'lsblk'
+efi_partition="${drive}1"
+root_partition="${drive}2"
+
+luks_label="cryptroot"
+luks_passphrase=""
+
+hostname="archlinux"
+timezone="" # run 'timedatectl list-timezones'
+username=""
+user_passphrase=""
+
+GPU_driver="mesa" # 'nvidia' or 'mesa'
+
+
 # Clean the TTY
 clear
 
