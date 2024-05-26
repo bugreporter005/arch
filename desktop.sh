75a19f9f9c147e8f3e63a6f6c21ff0c48687bd25
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-26 08:04:57 +0500

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -158,62 +158,62 @@ genfstab -U /mnt > /mnt/etc/fstab
 sed -i 's/subvolid=.*,//' /mnt/etc/fstab
 
 # Change root into the new system
-arch-chroot /mnt /bin/zsh -e << EOF
+#arch-chroot /mnt /bin/zsh -e << EOF
 
 # Set timezone based on IP address
-ln -sf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
-hwclock --systohc
+arch-chroot /mnt /bin/zsh ln -sf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
+arch-chroot /mnt /bin/zsh hwclock --systohc
 
 # Localization
-sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen
-sed -i "s/#ru_RU.UTF-8/ru_RU.UTF-8/" /etc/locale.gen
-sed -i "s/#kk_KZ.UTF-8/kk_KZ.UTF-8/" /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "FONT=${console_font}" > /etc/vconsole.conf
+arch-chroot /mnt /bin/zsh sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen
+arch-chroot /mnt /bin/zsh sed -i "s/#ru_RU.UTF-8/ru_RU.UTF-8/" /etc/locale.gen
+arch-chroot /mnt /bin/zsh sed -i "s/#kk_KZ.UTF-8/kk_KZ.UTF-8/" /etc/locale.gen
+arch-chroot /mnt /bin/zsh locale-gen
+arch-chroot /mnt /bin/zsh echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot /mnt /bin/zsh echo "FONT=${console_font}" > /etc/vconsole.conf
 
 # Network configuration
-echo "${hostname}" > /etc/hostname
+arch-chroot /mnt /bin/zsh echo "${hostname}" > /etc/hostname
 
 # Initramfs
-sed -i "s/MODULES=()/MODULES=(btrfs)/" /etc/mkinitcpio.conf
-sed -i "s/BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/" /etc/mkinitcpio.conf
-sed -i "s/HOOKS=(.*)/HOOKS=(base systemd plymouth autodetect microcode modconf sd-vconsole block sd-encrypt btrfs filesystems keyboard fsck)/" /etc/mkinitcpio.conf
-mkinitcpio -P
+arch-chroot /mnt /bin/zsh sed -i "s/MODULES=()/MODULES=(btrfs)/" /etc/mkinitcpio.conf
+arch-chroot /mnt /bin/zsh sed -i "s/BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/" /etc/mkinitcpio.conf
+arch-chroot /mnt /bin/zsh sed -i "s/HOOKS=(.*)/HOOKS=(base systemd plymouth autodetect microcode modconf sd-vconsole block sd-encrypt btrfs filesystems keyboard fsck)/" /etc/mkinitcpio.conf
+arch-chroot /mnt /bin/zsh mkinitcpio -P
 
 # User management
-useradd -m -G wheel,libvert -s /bin/zsh ${username}
-echo -n ${user_passphrase} | passwd ${username}
-passwd --lock root
-echo "${username} ALL=(ALL:ALL) ALL" > /etc/sudoers.d/${username}
+arch-chroot /mnt /bin/zsh useradd -m -G wheel,libvert -s /bin/zsh ${username}
+arch-chroot /mnt /bin/zsh echo -n ${user_passphrase} | passwd ${username}
+arch-chroot /mnt /bin/zsh passwd --lock root
+arch-chroot /mnt /bin/zsh echo "${username} ALL=(ALL:ALL) ALL" > /etc/sudoers.d/${username}
 
 # Boot loader
-cd ~ && git clone https://aur.archlinux.org/grub-improved-luks2-git.git # patched GRUB2 with Argon2 support
-cd grub-improved-luks2-git
-echo -n ${user_passphrase} | su ${username}
-echo -n ${user_passphrase} | sudo makepkg -rsi --noconfirm
-exit
-cd ~ && rm -rf grub-improved-luks2-git
+arch-chroot /mnt /bin/zsh cd ~ && git clone https://aur.archlinux.org/grub-improved-luks2-git.git # patched GRUB2 with Argon2 support
+arch-chroot /mnt /bin/zsh cd grub-improved-luks2-git
+arch-chroot /mnt /bin/zsh echo -n ${user_passphrase} | su ${username}
+arch-chroot /mnt /bin/zsh echo -n ${user_passphrase} | sudo makepkg -rsi --noconfirm
+arch-chroot /mnt /bin/zsh exit
+arch-chroot /mnt /bin/zsh cd ~ && rm -rf grub-improved-luks2-git
 
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+arch-chroot /mnt /bin/zsh grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 
 DRIVE_UUID=$(blkid -o value -s UUID ${drive})
 ROOT_UUID=$(blkid -o value -s UUID ${root_part})
 
-sed -i "s/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/" /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="rd.luks.name=${DRIVE_UUID}=${luks_label} rd.luks.options=tries=3,discard,no-read-workqueue,no-write-workqueue root=UUID=${ROOT_UUID} rootflags=subvol=/@ rw quiet splash loglevel=3 rd.udev.log_priority=3"/' /etc/default/grub
+arch-chroot /mnt /bin/zsh sed -i "s/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/" /etc/default/grub
+arch-chroot /mnt /bin/zsh sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="rd.luks.name=${DRIVE_UUID}=${luks_label} rd.luks.options=tries=3,discard,no-read-workqueue,no-write-workqueue root=UUID=${ROOT_UUID} rootflags=subvol=/@ rw quiet splash loglevel=3 rd.udev.log_priority=3"/' /etc/default/grub
 
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt /bin/zsh grub-mkconfig -o /boot/grub/grub.cfg
 
 # Mirror setup and Pacman configuration
-reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-sed -i "s/#Color/Color/" /etc/pacman.conf
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5\nILoveCandy/" /etc/pacman.conf
+arch-chroot /mnt /bin/zsh reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+arch-chroot /mnt /bin/zsh sed -i "s/#Color/Color/" /etc/pacman.conf
+arch-chroot /mnt /bin/zsh sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5\nILoveCandy/" /etc/pacman.conf
 
 # Reboot
-exit
+arch-chroot /mnt /bin/zsh exit
 #umount -a
 #cryptsetup close ${luks_label}
 #reboot
 
-EOF
+#EOF
