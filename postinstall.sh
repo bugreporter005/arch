055f769968a01fd7e44f7658c2b944f060be41eb
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-26 08:40:25 +0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 
-source variables.sh
-
+source desktop.sh
 
 # Wi-Fi via NetworkManager
-nmcli dev wifi connect ${wifi_SSID} password ${wifi_passphrase} # add 'hidden yes' for hidden networks
+nmcli dev wifi connect ${wifi_ssid} \
+               password ${wifi_passphrase} # add 'hidden yes' for hidden networks
 echo -n ${user_passphrase} | sudo systemctl enable NetworkManager --now
-
 
 # AUR Helper
 cd ~ && git clone https://aur.archlinux.org/paru-bin.git
 cd ~/paru-bin/
+echo -n ${user_passphrase} | su ${username}
 echo -n ${user_passphrase} | sudo makepkg -rsi --noconfirm
+exit
 cd ~ && rm -Rf ~/paru-bin
 
+# GPU driver
+gpu_type=$(lspci)
+if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+    gpu_driver="nvidia-lts nvidia-settings nvidia-smi"
+elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+    gpu_driver="xf86-video-amdgpu"
+elif grep -E "Integrated Graphics Controller|Intel Corporation UHD" <<< ${gpu_type}; then
+    gpu_driver="libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa"
+fi
 
 # Additional packages
 # TODO: Check if paru works with sudo
 echo -n ${user_passphrase} | sudo paru --noconfirm -S \
+    ${gpu_driver} \
     wget2 \
     curl \
     man \
@@ -40,12 +51,11 @@ echo -n ${user_passphrase} | sudo paru --noconfirm -S \
     obs-studio \
     qemu-full virt-manager
 
-# Video driver
-if ${GPU_driver} == "nvidia"; then
+# Nvidia settings
+# TODO: Fix me
+if ${gpu_driver} == "nvidia"; then
     echo -n ${user_passphrase} | sudo pacman --noconfirm -S nvidia-lts nvidia-settings nvidia-smi
     #echo " nvidia-drm.modeset=1" >> /boot/loader/entries/arch.conf
     sudo sed -i "s/MODULES=(.*)/MODULES=(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /etc/mkinitcpio.conf
     sudo mkinitcpio -P
-else
-    echo -n ${user_passphrase} | sudo pacman -S mesa
 fi
