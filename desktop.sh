797b56d1c2c4edc1687811896236808ce48cc65f
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-27 18:04:04 +0500

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -162,7 +162,7 @@ sed -i 's/subvolid=.*,//' /mnt/etc/fstab
 arch-chroot /mnt chmod 700 /.cryptkey
 arch-chroot /mnt head -c 64 /dev/urandom > /.cryptkey/keyfile.bin
 arch-chroot /mnt chmod 600 /.cryptkey/keyfile.bin
-arch-chroot /mnt cryptsetup -v luksAddKey -i 1 ${root_part} /.cryptkey/keyfile.bin
+arch-chroot /mnt echo -n ${luks_passphrase} | cryptsetup -v luksAddKey -i 1 ${root_part} /.cryptkey/keyfile.bin
 
 # Set timezone based on IP address
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
