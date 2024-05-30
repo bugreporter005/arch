10ade891831701e5f87ac83264183ff8e54b94de
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-30 17:17:13 +0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -105,13 +105,9 @@ mount -o noatime,compress=zstd,commit=120,subvol=@snapshots /dev/mapper/${luks_l
 mount -o noatime,compress=no,nodatacow,subvol=@cryptkey /dev/mapper/${luks_label} /mnt/.cryptkey
 mount -o noatime,compress=no,nodatacow,subvol=@swap /dev/mapper/${luks_label} /mnt/swap
 
-# Create and activate a swap file based on RAM size
-ram_size=$(free -m | awk '/^Mem:/{print $2}')
-swap_size=$(( (ram_size + 1023) / 1024 )) # convert to gigabytes and round up
-if [ $swap_size -le 8 ]; then
-    swap_size=$((swap_size * 2)) # double if the size is less than or equal to 8 gigabytes
-fi
-btrfs filesystem mkswapfile --size ${swap_size}G --uuid clear /mnt/swap/swapfile
+# Swap file creation and activation for hibernation
+ram_size=$(( ( $(free -m | awk '/^Mem:/{print $2}') + 1023 ) / 1024 ))
+btrfs filesystem mkswapfile --size ${ram_size}G --uuid clear /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
 
 # Format and mount the EFI partition
