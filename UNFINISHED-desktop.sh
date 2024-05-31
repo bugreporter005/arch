11f909924ce5083e01b7aa4477b486026a2451e6
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-06-01 00:35:46 +0500

    Update UNFINISHED-desktop.sh

--- UNFINISHED-desktop.sh
+++ UNFINISHED-desktop.sh
@@ -280,13 +280,17 @@ arch-chroot /mnt sudo -u ${username} paru --noconfirm -S \
     qemu-full virt-manager
 
 # GPU driver detection and installation
-gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+gpu=$(lspci | grep 'VGA compatible controller')
+if grep -E "NVIDIA|GeForce" <<< ${gpu}; then
     gpu_driver="nvidia-lts nvidia-settings nvidia-smi"
-elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+elif grep -E "AMD|Radeon" <<< ${gpu}; then
     gpu_driver="mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils"
-elif grep -E "Integrated Graphics Controller|Intel Corporation UHD" <<< ${gpu_type}; then
+elif grep "Intel Corporation" <<< ${gpu}; then
     gpu_driver="mesa lib32-mesa vulkan-intel lib32-vulkan-intel libva-intel-driver libva-utils"
+elif grep "Intel Corporation" && grep -E "NVIDIA|GeForce" <<< ${gpu}; then
+    gpu_driver="mesa lib32-mesa vulkan-intel lib32-vulkan-intel libva-intel-driver libva-utils nvidia-lts nvidia-settings nvidia-smi"
+elif grep -E "AMD|Radeon" && grep -E "NVIDIA|GeForce" <<< ${gpu}; then
+    gpu_driver="mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver libva-utils nvidia-lts nvidia-settings nvidia-smi"
 fi
 if [ -n $gpu_driver ] then;
     arch-chroot /mnt pacman -S ${gpu_driver} --noconfirm
