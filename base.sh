1d85fa322c27f88231201ad642c77711b158d360
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-05-31 17:12:56 +0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -224,7 +224,7 @@ zram-size = ram * 2
 compression-algorithm = zstd
 EOF
     arch-chroot /mnt systemctl daemon-reload
-    arch-chroot /mnt systemctl enable --now systemd-zram-setup@zram0.service
+    arch-chroot /mnt systemctl start systemd-zram-setup@zram0.service
 fi
 
 # Bootloader
