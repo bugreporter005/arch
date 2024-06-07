f49c40cb13623a7bfd3ab7216b3f6e048e59793e
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-06-08 02:51:33 +0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -208,19 +208,7 @@ arch-chroot /mnt locale-gen
 
 cat > /mnt/etc/locale.conf << EOF
 LANG=en_US.UTF-8
-LANGUAGE=en_US:en_GB:en
-LC_ADDRESS=en_US.UTF-8
-LC_COLLATE=en_US.UTF-8
-LC_CTYPE=en_US.UTF-8
-LC_IDENTIFICATION=en_US.UTF-8
 LC_MEASUREMENT=en_GB.UTF-8
-LC_MESSAGES=en_US.UTF-8
-LC_MONETARY=en_US.UTF-8
-LC_NAME=en_US.UTF-8
-LC_NUMERIC=en_US.UTF-8
-LC_PAPER=en_US.UTF-8
-LC_TELEPHONE=en_US.UTF-8
-LC_TIME=en_US.UTF-8
 EOF
 
 echo "FONT=${console_font}" > /mnt/etc/vconsole.conf
