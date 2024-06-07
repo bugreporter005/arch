4a435e1e0a5e1adf373cab96dcf94ddf8968d3d0
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-06-08 04:29:18 +0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -46,7 +46,7 @@ fi
 
 # Check if Secure Boot is enabled in BIOS
 setup_mode=$(bootctl status | grep -E "Secure Boot.*setup" | wc -l)
-if [[ $setup_mode -ne 1 ]] ; then
+if [ $setup_mode -ne 1 ] ; then
     echo "The firmware is not in the setup mode. Please check BIOS."
     exit 1
 fi
