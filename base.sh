41d3373de363905eb31ed936199f843d95fb1e32
bugreporter005 <167757985+bugreporter005@users.noreply.github.com>
2024-06-11 00:25:26 +0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -332,7 +332,7 @@ ROOT_UUID=$(blkid -o value -s UUID ${root_part})
 RESUME_OFFSET=$(btrfs inspect-internal map-swapfile -r /mnt/swap/swapfile)
 
 sed -i "/GRUB_ENABLE_CRYPTODISK=y/s/^#//" /mnt/etc/default/grub
-sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/rd.luks.name=${ROOT_UUID}=${luks_label} rd.luks.options=tries=3,discard,no-read-workqueue,no-write-workqueue root=\/dev\/mapper\/${luks_label} rootflags=subvol=\/@ rw cryptkey=rootfs:\/root\/.cryptkey\/keyfile.bin quiet splash loglevel=3 rd.udev.log_priority=3 resume=\/dev\/mapper\/${luks_label} resume_offset=${RESUME_OFFSET}\"/" /mnt/etc/default/grub
+sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\".*\"|GRUB_CMDLINE_LINUX_DEFAULT=\"rd.luks.name=${ROOT_UUID}=${luks_label} rd.luks.options=tries=3,discard,no-read-workqueue,no-write-workqueue root=/dev/mapper/${luks_label} rootflags=subvol=/@ rw cryptkey=rootfs:/root/.cryptkey/keyfile.bin quiet splash loglevel=3 rd.udev.log_priority=3 resume=/dev/mapper/${luks_label} resume_offset=${RESUME_OFFSET}\"|" /mnt/etc/default/grub
 
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
